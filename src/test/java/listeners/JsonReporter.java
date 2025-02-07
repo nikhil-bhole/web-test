f9e747c3c82dba3fce479b9a75e227f4d73e46b7
nikhil <nikhilbhole.0419@gmail.com>
2025-02-07 22:10:07 +0530

    custom reporting

--- src/test/java/listeners/JsonReporter.java
+++ src/test/java/listeners/JsonReporter.java
@@ -16,7 +16,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.ReentrantLock;
 
